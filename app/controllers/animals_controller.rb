2dc682291dcfa9caf6bd942f6a2ec3c43152b8c8
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-06-27 03:01:54 -0700

    create actions

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,14 +1,27 @@
 class AnimalsController < ApplicationController
 
-    def Index
+    def index
         animals = Animal.all
         render json: animals
     end
 
     def show
-        animal = Animal.find(params[:id])
+        animals = Animal.find(params[:id])
         render json: animal
+    end
 
+    def create
+        animals = Animal.create(animal_params)
+        if animals.valid?
+        render json: animals
+        else
+            render json: animals.errors
+        end
+    end
 
+    private
+    def animal_params
+        params.require(:animal).permit(:common_name, :latin_name, :kingdom)
+    end
 
 end
